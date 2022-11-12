0c39767a80c5a041ad93b9a420b28410bec80e8c
Gabriel <gabrieljaimecanooquendo@gmail.com>
2022-11-12 08:53:32 -0500

    a

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {Login} from './components/Login';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+      initialRouteName="Login"
+      >
+        <Stack.Screen name = "Login" component={Login}/>
+        <Stack.Screen name = "Registrar" component={Registrar} options={{title:'Registrar usuario'}}/> 
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
