bd79ad3666cc110a1c056fa18dabc4b4040385e8
alexcode737 <alexis15torres16zorrilla@gmail.com>
2022-11-13 14:49:38 -0500

    Se corrige el formulario para registrar ventas

--- components/registroVenta.jsx
+++ components/registroVenta.jsx
@@ -1,7 +1,9 @@
-import { Text,View,TextInput,FlatList } from 'react-native'
-import {useForm,Controller} from 'react-hook-form'
+import { Text,View,TextInput,FlatList } from 'react-native';
+import axios from 'axios'; // Es un consumidor de apis
+import {useForm,Controller} from 'react-hook-form';
 
 export default function RegistroVenta(){
+
     const {register,control,handleSubmit,formState:{errors}}=useForm({
         defaultValues:{
             idvend:'',
@@ -22,34 +24,48 @@ export default function RegistroVenta(){
         }
     ]
 
+    const registrarVenta = () => {
+        let fecha = new Date()
+    }
+
     return(
         <View>
-            <Controller
-                control={control}
-                rules={{
-                    required:true,
-                    pattern:/^[0-9{8,12}]+$/g,
-                    maxLength:30,
-                    minLength:3
-                }}
-                render={({field:{onChange,onBlur,value}})=>(
-                    <TextInput
-                        style={[styles.inputs,{borderColor:errors.fullname?.type == "required"||errors.fullname?.type == "maxLength"
-                        ||errors.fullname?.type == "minLength"||errors.fullname?.type == "pattern"?'red':'green'}]}
-                        placeholder="identificacion"
-                        onChange={onChange}
-                        onBlur={onBlur}
-                        value={value}               
-                    />
-                )}
-                name='idvend'
-            />
-
             <FlatList
                 data={zonas}
                 renderItem={renderItem}
                 keyExtractor={({ zona }, index) => zona}
             />
+
+           <Controller
+                control={control}
+                rules={{
+                    required:true,
+                    maxLength:12,
+                    minLength:3
+                }}
+                render={({field:{onChange,onBlur,value}})=>(
+                    <TextInput
+                        style={[styles.inputs,{borderColor:errors.valorVenta ?.type == 'required' ||
+                        errors.valorVenta ?.type == 'pattern' || errors.valorVenta ?.type == 'maxLength' ||
+                        errors.valorVenta ?.type == 'minLength' ? 'red' : 'green' }]}
+                        placeholder='Valor de venta'
+                        onChange={onChange}
+                        onBlur={onBlur}
+                        value={value}
+                    />
+                )}
+                name='valorVenta'
+           />
+
+           {errors.valorVenta ?.type == 'required' && <Text style={{color:'red'}}> El campo es requerido </Text>}
+           {errors.valorVenta ?.type == 'maxLength' && <Text style={{color:'red'}}> Debe contener maximo 12 caracteres</Text>}
+           {errors.valorVenta ?.type == 'minLength' && <Text style={{color:'red'}}>Debe contener minimo 3 caracteres</Text>} 
+
+            <TouchableOpacity
+                onPress={()=>registrarVenta()}
+            >
+                <Text style={{color:'white'}}>Añadir</Text>
+            </TouchableOpacity>
         </View>
     )
 }
