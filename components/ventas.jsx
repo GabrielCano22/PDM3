611e50b3fac2b582887791b06a956daf681fbf18
BrayanRestrepo <brestrepoisaza@gmail.com>
2022-11-13 16:47:32 -0500

    flat ventas

---
+++ components/ventas.jsx
@@ -0,0 +1,45 @@
+import { FlatList,TouchableOpacity,View } from "react-native-web";
+import { Helpers } from "../helpers/Helpers";
+import Textrows from './Textrows';
+import { userv } from "./Homev";
+
+
+export default function ventas ({navigation,route}) {
+    let id = ''
+    if (route.params.idvend != null){
+        id = route.params.idvend;
+    }else{
+        id = userv.idvend;
+    }
+    let objt = new Helpers();
+    let data = objt.getVentasUser(id);
+
+    return(
+        <FlatList
+          data={data}
+          //keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (     
+                <View>
+                    <Textrows
+                    label = "fecha:"
+                    dato = {item.fecha}
+                    />
+                    <View>
+                        <Textrows
+                            label = "Zona:"
+                            dato = {item.zona}
+                        />
+                        <Textrows
+                            label = "valor:"
+                            dato = {item.ValorVenta}
+                        />
+                    </View>
+                    <Textrows
+                        label = "Comision:"
+                        dato = {item.comision}
+                    />
+                </View>   
+          )}
+        />
+    )
+}
